aa6afc5d843fafae89ae6eadc0159eaa7e570006
ArthurNaue <arthur_naue@Hotmail.com>
2023-10-28 14:34:45 -0300

    diminui um pouco mais a velocidade do boss

--- main.py
+++ main.py
@@ -18,79 +18,79 @@ def draw_text(text, font, x, y):
     screen.blit(img, (x, y))
 
 
-def ler_high_score():
+def read_high_score():
     with open("high score.txt", "r") as f:
         return f.read()
 
 
-def sair():
+def quit():
     pygame.quit()
     sys.exit()
 
 
 def main_menu():
     running = True
-    botao1 = pygame.image.load("Images/botao.png")
-    botao2 = pygame.image.load("Images/botao.png")
-    botao3 = pygame.image.load("Images/botao.png")
-    botao1_rect = botao1.get_rect()
-    botao2_rect = botao2.get_rect()
-    botao3_rect = botao3.get_rect()
-    botao1_rect.topleft = (250, 300)
-    botao2_rect.topleft = (250, 400)
-    botao3_rect.topleft = (250, 500)
+    button1 = pygame.image.load("Images/botao.png")
+    button2 = pygame.image.load("Images/botao.png")
+    button3 = pygame.image.load("Images/botao.png")
+    buttonRect1 = button1.get_rect()
+    buttonRect2 = button2.get_rect()
+    buttonRect3 = button3.get_rect()
+    buttonRect1.topleft = (250, 300)
+    buttonRect2.topleft = (250, 400)
+    buttonRect3.topleft = (250, 500)
     font = pygame.font.Font(None, 36)
-    texto = False
-    texto1 = "JOGAR"
-    texto2 = "AJUDA"
-    texto3 = "SAIR"
-    texto1_surface = font.render(texto1, True, (0, 0, 0))
-    texto1_rect = texto1_surface.get_rect()
-    texto1_rect.center = botao1_rect.center
-    texto2_surface = font.render(texto2, True, (0, 0, 0))
-    texto2_rect = texto2_surface.get_rect()
-    texto2_rect.center = botao2_rect.center
-    texto3_surface = font.render(texto3, True, (0, 0, 0))
-    texto3_rect = texto3_surface.get_rect()
-    texto3_rect.center = botao3_rect.center
-    titulo = pygame.image.load("Images/titulo.png")
-    botao4 = pygame.image.load("Images/botao2.png")
-    botao4_rect = botao4.get_rect()
-    botao4_rect.topleft = (100, 350)
-    high_score = int(ler_high_score())
-    texto4 = str(high_score)
-    texto4_surface = font.render(texto4, True, (0, 0, 0))
-    texto4_rect = texto4_surface.get_rect()
-    texto4_rect.center = botao4_rect.center
+    text = False
+    text1 = "JOGAR"
+    text2 = "AJUDA"
+    text3 = "SAIR"
+    textSur1 = font.render(text1, True, (0, 0, 0))
+    textRect1 = textSur1.get_rect()
+    textRect1.center = buttonRect1.center
+    textSur2 = font.render(text2, True, (0, 0, 0))
+    textRect2 = textSur2.get_rect()
+    textRect2.center = buttonRect2.center
+    textSur3 = font.render(text3, True, (0, 0, 0))
+    textRect3 = textSur3.get_rect()
+    textRect3.center = buttonRect3.center
+    tittle = pygame.image.load("Images/titulo.png")
+    button4 = pygame.image.load("Images/botao2.png")
+    buttonRect4 = button4.get_rect()
+    buttonRect4.topleft = (100, 350)
+    highScore = int(read_high_score())
+    text4 = str(highScore)
+    textSur4 = font.render(text4, True, (0, 0, 0))
+    textRect4 = textSur4.get_rect()
+    textRect4.center = buttonRect4.center
 
     while running:
-        if pygame.mouse.get_pressed()[0] and botao1_rect.collidepoint(
+        if pygame.mouse.get_pressed()[0] and buttonRect1.collidepoint(
             pygame.mouse.get_pos()
         ):
             game()
-        if pygame.mouse.get_pressed()[0] and botao2_rect.collidepoint(
+        if pygame.mouse.get_pressed()[0] and buttonRect2.collidepoint(
             pygame.mouse.get_pos()
         ):
-            texto = True
-        if pygame.mouse.get_pressed()[0] and botao3_rect.collidepoint(
+            text = True
+        if pygame.mouse.get_pressed()[0] and buttonRect3.collidepoint(
             pygame.mouse.get_pos()
         ):
-            sair()
+            quit()
 
         screen.blit(background, (0, 0))
-        screen.blit(botao1, (250, 300))
-        screen.blit(botao2, (250, 400))
-        screen.blit(botao3, (250, 500))
-        screen.blit(texto1_surface, texto1_rect)
-        screen.blit(texto2_surface, texto2_rect)
-        screen.blit(texto3_surface, texto3_rect)
-        screen.blit(botao4, (100, 350))
+        screen.blit(button1, (250, 300))
+        screen.blit(button2, (250, 400))
+        screen.blit(button3, (250, 500))
+        screen.blit(textSur1, textRect1)
+        screen.blit(textSur2, textRect2)
+        screen.blit(textSur3, textRect3)
+        screen.blit(textSur4, textRect4)
+        screen.blit(button4, (100, 350))
         draw_text("HIGH", pygame.font.Font(None, 30), 125, 360)
         draw_text("SCORE", pygame.font.Font(None, 30), 115, 380)
-        screen.blit(texto4_surface, texto4_rect)
-        screen.blit(titulo, (50, -100))
+        screen.blit(tittle, (50, -100))
 
-        if texto:
+        if text:
             draw_text("+ PONTOS = + RAPIDO", pygame.font.Font(None, 30), 360, 390)
             draw_text("CLIQUE PARA MATAR", pygame.font.Font(None, 30), 368, 360)
             draw_text("SE TE PEGAREM", pygame.font.Font(None, 30), 390, 420)
@@ -98,7 +98,7 @@ def main_menu():
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                sair()
+                quit()
 
         pygame.display.update()
 
@@ -163,7 +163,7 @@ def game():
     pygame.time.set_timer(spawn, 3000)
 
     try:
-        high_score = int(ler_high_score())
+        high_score = int(read_high_score())
     except:
         high_score = 0
 
@@ -240,7 +240,7 @@ def game():
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                sair()
+                quit()
             elif event.type == spawn:
                 if inimigo_spawn == False:
                     inimigo_spawn = True
@@ -280,7 +280,7 @@ def game():
                         if contagem_boss == 2:
                             boss_tipo = olho
                             boss_vida = 8
-                            boss_velocidade = 0.05
+                            boss_velocidade = 0.04
                         if contagem_boss >= 3:
                             boss_tipo = random.choice(lista_boss)
                             if boss_tipo == ceifador:
@@ -288,7 +288,7 @@ def game():
                                 boss_velocidade = 0.35
                             if boss_tipo == olho:
                                 boss_vida = 8
-                                boss_velocidade = 0.05
+                                boss_velocidade = 0.04
                         boss = Inimigos(
                             boss_tipo,
                             boss_vida,
@@ -360,7 +360,7 @@ def game_over():
     texto2_rect = texto2_surface.get_rect()
     texto2_rect.center = botao2_rect.center
     titulo = pygame.image.load("Images/game_over.png")
-    high_score = int(ler_high_score())
+    high_score = int(read_high_score())
     texto3 = str(high_score)
     texto3_surface = font.render(texto3, True, (0, 0, 0))
     texto3_rect = texto3_surface.get_rect()
@@ -374,7 +374,7 @@ def game_over():
         if pygame.mouse.get_pressed()[0] and botao2_rect.collidepoint(
             pygame.mouse.get_pos()
         ):
-            sair()
+            quit()
 
         screen.blit(background, (0, 0))
         screen.blit(botao1, (250, 300))
@@ -389,7 +389,7 @@ def game_over():
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                sair()
+                quit()
 
         pygame.display.update()
 
